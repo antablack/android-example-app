625e46745a9355f652d04298eedf25085945702a
Juan Sanchez <juan.sanchez@ultertechnologies.com>
2019-11-15 10:55:49 -0500

    [feat]: Add toast

--- app/src/main/java/com/example/exampleapp/MainActivity.kt
+++ app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     fun btnOnClick(view: View): Unit {
         Log.i("Name", edt.text.toString())
+        val message = edt.text.toString()
+        Toast.makeText(this, "Hi there $message", Toast.LENGTH_LONG).show()
+
     }
 
 
